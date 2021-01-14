a3b704b8618cf3c742e60bad9f5c20ac2a15cc04
vaidasuope <vaida.dabasinskaite@gmail.com>
2021-01-14 08:22:18 +0200

    first

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+$products = [
+    'Products' => [
+        'IT' => [
+            'MackBook',
+            'USB hub',
+            'Keyboard'],
+        'Other'=>[
+            'Samsung Galaxy',
+            'Apple watch'
+        ]
+    ],
+    'Service' => [
+        'IT'=> [
+            'WEB development',
+            'WEB design'
+        ],
+        'Business' => [
+            'Company registration',
+            'SEO optimization'
+        ]
+    ]
+];
