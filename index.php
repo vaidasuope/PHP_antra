61f45ad8e688de7d71c595c61f3185c8e8fca19d
vaidasuope <vaida.dabasinskaite@gmail.com>
2021-01-14 08:45:09 +0200

    second

--- index.php
+++ index.php
@@ -22,3 +22,21 @@ $products = [
         ]
     ]
 ];
+
+?>
+
+<ul>
+    <?php foreach ($products as $key => $value):?>
+        <li><?=$key;?>:<?=$value;?></li>
+    <ul>
+        <?php foreach ($value as $item => $next):?>
+        <li><?=$item;?><?=$next;?></li>
+        <ul>
+            <?php foreach ($next as $nextItem => $nextValue):?>
+                <li><?=$nextItem;?><?=$nextValue;?></li>
+            <?php endforeach;?>
+        </ul>
+        <?php endforeach;?>
+    </ul>
+        <?php endforeach;?>
+</ul>
