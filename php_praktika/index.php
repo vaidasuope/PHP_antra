27fbea4fa97c47374d3b0e0dc47f86ee28944462
vaidasuope <vaida.dabasinskaite@gmail.com>
2021-01-14 09:46:14 +0200

    next one

---
+++ php_praktika/index.php
@@ -0,0 +1,58 @@
+<?php
+
+$schoolData = [
+    '5a' => [
+        'rand ()' => [
+            'petras',
+            'petraitis',
+            'ivertinimai' => [5, 9, 10]
+        ],
+        'rand ()' => [
+            'jonas',
+            'jonaitis',
+            'ivertinimai' => [6, 7, 8]
+        ],
+        'rand ()' => [
+            'reda',
+            'redaite',
+            'ivertinimai' => [8, 6, 9]
+        ],
+        'rand ()' => [
+            'simas',
+            'simaitis',
+            'ivertinimai' => [10, 7, 6]
+        ],
+        'rand ()' => [
+            'simas',
+            'simaitis',
+            'ivertinimai' => [10, 7, 6]
+        ]
+    ],
+    '5b' => [
+        'rand ()' => [
+            'vaida',
+            'vaidaite',
+            'ivertinimai' => [5, 9, 8]
+        ],
+        'rand ()' => [
+            'ausra',
+            'ausraite',
+            'ivertinimai' => [6, 10, 8]
+        ],
+        'rand ()' => [
+            'kostas',
+            'kostaitis',
+            'ivertinimai' => [8, 7, 6]
+        ],
+        'rand ()' => [
+            'laima',
+            'laimaite',
+            'ivertinimai' => [8, 7, 9]
+        ],
+        'rand ()' => [
+            'mantas',
+            'simaitis',
+            'ivertinimai' => [10, 7, 8]
+        ],
+    ]
+];
